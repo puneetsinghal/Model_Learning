5d65c81839ddc02fa8da4bc6014760959725c7b3
puneetsinghal <puneetatpks@gmail.com>
2018-02-26 18:38:50 -0500

    working training and testing modes with 4 different robot {planarRR, planarRRR, nonPlanarRRR, nonPlanarRRRRR}

--- kinematics/gaussianProcesses/gp_fk.py
+++ kinematics/gaussianProcesses/gp_fk.py
@@ -39,83 +39,102 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-	# Parameters
-	params = {}
-	results = {}
-	if(args.robot == "planarRR"):
-		params['dof'] = 2
-		params['numOutput'] = 2
-		params['linkLengths'] = np.array([1., 1.])
-		params['batchSize'] = 100
-		params['optimizationStep'] = 1
-		robot = PlanarRR(params['dof'], params['linkLengths'])
+	if(args.mode == 'train'):
+		# Parameters
+		params = {}
+		results = {}
+		if(args.robot == "planarRR"):
+			params['dof'] = 2
+			params['numOutput'] = 2
+			params['linkLengths'] = np.array([1., 1.])
+			params['batchSize'] = 100
+			params['optimizationStep'] = 1
+			robot = PlanarRR(params['dof'], params['linkLengths'])
 
-	if(args.robot == "planarRRR"):
-		params['dof'] = 3
-		params['numOutput'] = 2
-		params['linkLengths'] = np.array([1., 1., 1.])
-		params['batchSize'] = 1000
-		params['optimizationStep'] = 1
-		robot = PlanarRRR(params['dof'], params['linkLengths'])
+		if(args.robot == "planarRRR"):
+			params['dof'] = 3
+			params['numOutput'] = 2
+			params['linkLengths'] = np.array([1., 1., 1.])
+			params['batchSize'] = 1000
+			params['optimizationStep'] = 1
+			robot = PlanarRRR(params['dof'], params['linkLengths'])
+			
+		if(args.robot == "nonPlanarRRR"):
+			params['dof'] = 3
+			params['numOutput'] = 3
+			params['linkLengths'] = np.array([1., 1., 1.])
+			params['batchSize'] = 1000
+			params['optimizationStep'] = 1
+			robot = NonPlanarRRR(params['dof'], params['linkLengths'])
+			
+		if(args.robot == "nonPlanarRRRRR"):
+			params['dof'] = 5
+			params['numOutput'] = 3
+			params['batchSize'] = 10000
+			params['optimizationStep'] = 1
+			d1 = 0.14
+			d4 = 0.11
+			d5 = 0.075 + 0.15
+			params['dh_parameters'] = np.array([[0, pi/2, d1, 0],
+								    [0.334, pi, 0, 0],
+								    [0.334, pi, 0, 0],
+								    [0, pi/2, d4, pi/2],
+								    [0, 0, d5, 0]])
+			robot = NonPlanarRRRRR(params['dof'], params['dh_parameters'])
 		
-	if(args.robot == "nonPlanarRRR"):
-		params['dof'] = 3
-		params['numOutput'] = 3
-		params['linkLengths'] = np.array([1., 1., 1.])
-		params['batchSize'] = 1000
-		params['optimizationStep'] = 1
-		robot = NonPlanarRRR(params['dof'], params['linkLengths'])
+		params['tolerance'] = 1*1e-4
+		print(params)
+		robot.numOutput = params['numOutput']
+
+		X, Y = robot.generateData(params['batchSize'])
 		
-	if(args.robot == "nonPlanarRRRRR"):
-		params['dof'] = 5
-		params['numOutput'] = 3
-		params['batchSize'] = 10000
-		params['optimizationStep'] = 1
-		d1 = 0.14
-		d4 = 0.11
-		d5 = 0.075 + 0.15
-		params['dh_parameters'] = np.array([[0, pi/2, d1, 0],
-							    [0.334, pi, 0, 0],
-							    [0.334, pi, 0, 0],
-							    [0, pi/2, d4, pi/2],
-							    [0, 0, d5, 0]])
-		robot = NonPlanarRRRRR(params['dof'], params['dh_parameters'])
-	
-	params['tolerance'] = 1*1e-4
-	print(params)
-	robot.numOutput = params['numOutput']
+		startTime = time.time()
+		kernel = C(1.0, (1e-3, 1e3)) * RBF(12, (1e-2, 1e2))
+		gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=params['optimizationStep'])
+		gp.fit(X, Y)
 
-	X, Y = robot.generateData(params['batchSize'])
-	
-	startTime = time.time()
-	kernel = C(1.0, (1e-3, 1e3)) * RBF(12, (1e-2, 1e2))
-	gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=params['optimizationStep'])
-	gp.fit(X, Y)
+		endLearningTime = time.time()
+		results['trainingTime'] = endLearningTime - startTime
+		
+		testBatchX, testBatchY = robot.generateTrajectory()
 
-	endLearningTime = time.time()
-	results['trainingTime'] = endLearningTime - startTime
+		y_pred, sigma = gp.predict(testBatchX, return_std=True)
+		endTestTime = time.time()
+		results['testingTime'] = endTestTime - endLearningTime
+		
+		results['errorVector'] = np.sqrt(np.sum((y_pred - testBatchY)**2, 1))
+		results['averageCost'] = np.mean(results['errorVector'])
+		results['reward'] = 100.0/robot.testSize*np.sum(results['errorVector'] < params['tolerance'])
+
+		filename = './train_log/' + args.robot +'_' + make_log_dir('./train_log/')
+		pickle.dump([params, robot, gp, results], open(filename, 'wb'))
+
+	if(args.mode == 'test'):
+		startTime = time.time()
+		# load the data [params, gp, results] from disk
+		filename = args.model
+		params, robot, gp, results = pickle.load(open(filename, 'rb'))
+		robot.numOutput = params['numOutput']
+		testBatchX, testBatchY = robot.generateTrajectory()
+
+		y_pred, sigma = gp.predict(testBatchX, return_std=True)
+		
+		endTestTime = time.time()
+		results['testingTime'] = endTestTime - startTime
+		
+		results['errorVector'] = np.sqrt(np.sum((y_pred - testBatchY)**2, 1))
+		results['averageCost'] = np.mean(results['errorVector'])
+		results['reward'] = 100.0/robot.testSize*np.sum(results['errorVector'] < params['tolerance'])
+
 	print("The total time taken for learning is :{} seconds".format(results['trainingTime']))
-
-	testBatchX, testBatchY = robot.generateTrajectory()
-
-	y_pred, sigma = gp.predict(testBatchX, return_std=True)
-	endTestTime = time.time()
-	results['testingTime'] = endTestTime - endLearningTime
 	print("The total time taken for testing is :{} seconds".format(results['testingTime']))
 
-	results['errorVector'] = np.sqrt(np.sum((y_pred - testBatchY)**2, 1))
-	results['averageCost'] = np.mean(results['errorVector'])
-	results['reward'] = 100.0/robot.testSize*np.sum(results['errorVector'] < params['tolerance'])
-
-	filename = './train_log/' + args.robot +'_' + make_log_dir('./train_log/')
-	pickle.dump([params, gp, results], open(filename, 'wb'))
-	
 	print("Success rate = {}".format(results['reward']))
 	print("Average error is:{}".format(results['averageCost']))
 
 	fig = plt.figure(1)
 	ax = fig.add_subplot(111, projection='3d')
-	if(robot.numOutput == 2):	
+	if(params['numOutput'] == 2):	
 		ax.scatter(testBatchY[:,0], testBatchY[:,1], 0.0*testBatchY[:,1], 'g*')
 		ax.scatter(y_pred[:,0], y_pred[:,1], 0.0*y_pred[:,1], 'r*')
 	else:
